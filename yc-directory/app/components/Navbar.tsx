45b15d0279dd6a039170f71bb4b416e5dae07976
zelgaliz215 <zelyhobit@gmail.com>
2025-01-10 20:29:03 -0500

    Creacion de Server actions para login y logout mediante el proveedor de github usando el api de authjs

--- yc-directory/app/components/Navbar.tsx
+++ yc-directory/app/components/Navbar.tsx
@@ -6,11 +6,13 @@ import { auth, signIn, signOut } from "../auth";
 
 const Navbar = async () => {
 
+  // variable sesion creada mediante authjs para manejar los datos traidos del proveedor en el caso github
   const session = await auth();
 
   return (
     <header className="px-5 py-3 bg-white shadow-sm font-work-sans">
       <nav className="flex justify-between items-center">
+        
         {/* Logo */}
         <Link href="/">
           <Image src="/logo.png" alt="logo" width={144} height={30} />
@@ -24,23 +26,38 @@ const Navbar = async () => {
                 <Link href="/startup/create">
                   <span>Create</span>
                 </Link>
-
-                <button onClick={signOut}>
-                  <span>Logout</span>
-                </button>
-
+                
+                {/* Server action  Para hacer login */}
+                <form action={async () => {
+                  "use server"
+                  // Cierra la Sesion y redirecciona a la raiz del app
+                  await signOut( {redirectTo: "/"})
+                }}>
+                  <button type="submit">
+                    <span>Logout</span>
+                  </button>
+                </form>
+                
+                {/*! Cambiar el id sesion----------------------------------------------------------------------------------  */}
                 <Link href={`/user/${session?.id}`}>
-                  <span>{session?.user?.name}</span>
+                  <span>{ session?.user?.name }</span>
                 </Link>
-
               </>
-            ) : (
-              <button onClick={signIn('github')}>
-                <span>Login</span>
-              </button>
-            )}
+            )
+              : (
+                // Server action  Para hacer login que se renderiza si la sesion o el usuario no existe
+                <form action= {async () => {
+                  "use server";
+                  // Se invoca github opara crear la sesion mediante el boton de login
+                  await signIn('github');
+                }}>
+                    <button type="submit">
+                        Login
+                    </button>
+                </form>
+              )}
+          
         </div>
-
       </nav>
     </header >
   );
