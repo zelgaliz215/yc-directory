bb3eccbe174ddd69af5ffb6f8d2bcffd78018295
zelgaliz215 <zelyhobit@gmail.com>
2025-01-11 08:38:35 -0500

    Creada la secciÂón Hero, falta configurar los botones superior e inferior

--- yc-directory/app/(root)/page.tsx
+++ yc-directory/app/(root)/page.tsx
@@ -1,7 +1,12 @@
 export default function Home() {
   return (
     <>
-      <h1 className="heading">Home</h1>
+      <section className="pink_container">
+        <h1 className="heading">Pitch Your Startup, <br /> Connect With Entrepreneurs</h1>
+
+        <p className="sub-heading !max-w-3xl">Submit Ideas, Vote on Pitches, and Get Noticed in Virtual Competitions</p>
+      </section>
+
     </>
   );
 }
